e723c1d305a0ede9c1616cb23bd74ca8acbff6a4
Marcin Michalski <evulmastah@gmail.com>
2019-06-12 21:19:34 +0200

    Make logger file optional & log to iostream by default

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
 #include "game.hpp"
 #include "game_state_start.hpp"
-#include "logger.hpp"
 
 int main() {
-    g_logger->setLogFilePath("log.txt");
-
     Game game;
     game.pushState(new GameStateStart(&game));
     game.run();
